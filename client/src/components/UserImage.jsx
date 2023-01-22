e38853b08edda46432a05bd2006436f8d443743b
ciijay99 <chigozieahanwa99@gmail.com>
2023-01-22 17:42:29 +0100

    Final

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = '60px' }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`https://social-media-backend-x66u.onrender.com/assets/${image}`}
       />
     </Box>
   )
